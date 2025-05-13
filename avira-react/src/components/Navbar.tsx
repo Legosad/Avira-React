3fa7ba86946d8f1d8c607b7a59011eb64a0daf4a
Legosad <saadaltaf1202@gmail.com>
2025-05-13 16:04:06 -0400

    Navbar finished

--- avira-react/src/components/Navbar.tsx
+++ avira-react/src/components/Navbar.tsx
@@ -1,18 +1,21 @@
 import logo from "../assets/Logo.png"
-import { FaUser } from "react-icons/fa"
+import { FaUser, FaHeart } from "react-icons/fa"
+import {FaCartShopping} from "react-icons/fa6"
 export const Navbar = () => {
   return (
-      <div className="flex">
-      <div className="image-container"><img src={logo} alt="" /></div>
-      <nav>
-        <li>Home</li>
-        <li>Shop</li>
-        <li>Blogs</li>
-        <li>About </li>
-        <li>Contact</li>
+      <div className="flex justify-around items-center py-2 border-b">
+      <div className="image-container"><img src={logo} alt="" width={60}/></div>
+      <nav className="flex gap-8 justify-center items-center">
+        <a>Home</a>
+        <a>Shop</a>
+        <a>Blogs</a>
+        <a>About </a>
+        <a>Contact</a>
       </nav>
-      <div className="user">
-        <p><FaUser /> Login/Register</p>
+      <div className="user flex gap-8 text-[#db6b97] text-bold">
+        <div className="flex gap-2 justify-center items-center"><FaUser /> Login/Register</div>
+        <div className="flex gap-2 justify-center items-center"><FaHeart /> 0 </div>
+        <div className="flex gap-2 justify-center items-center"><FaCartShopping /> 0 </div>
       </div>
     </div>
   )
