5afabe3748c8f62601bdd153a921b3a50ab4926a
Legosad <saadaltaf1202@gmail.com>
2025-08-25 16:39:45 +0500

    server side added

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => {
+    res.send("API running")
+})
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000")
+})
